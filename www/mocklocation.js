1b5d97d33e7ea0e3be2db0b33379a84156385f5f
abdielalvin <abdiel.alvin@yahoo.com>
2019-08-09 13:59:46 +0700

    Update mocklocation.js

--- www/mocklocation.js
+++ www/mocklocation.js
@@ -1,12 +1,17 @@
-var mocklocation = {
-  check: function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'MockLocationChecker', 'check', []);
-  }
+// Empty constructor
+function MockLocationChecker() {}
+
+// The function that passes work along to native shells
+MockLocationChecker.prototype.check = function(successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, 'MockLocationChecker', 'check', []);
 }
 
-cordova.addConstructor(function () {
-  if (!window.plugins) {window.plugins = {};}
-
-  window.plugins.mocklocationchecker = mocklocation;
+// Installation constructor that binds MockLocationChecker to window
+MockLocationChecker.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.mocklocationchecker = new MockLocationChecker();
   return window.plugins.mocklocationchecker;
-});
+};
+cordova.addConstructor(MockLocationChecker.install);
