b1fa69a9573df795c0d3204b5982624f5aed24b4
abdielalvin <abdiel.alvin@yahoo.com>
2019-08-09 13:42:55 +0700

    Update MockLocationChecker.java

--- src/android/com/diegorribeiro/MockLocationChecker.java
+++ src/android/com/diegorribeiro/MockLocationChecker.java
@@ -26,8 +26,7 @@ public class MockLocationChecker extends CordovaPlugin{
 
     private int MY_PERMISSIONS_REQUEST = 0;
 
-    private JSONArray arrayGPS = new JSONArray();
-    private JSONObject objGPS = new JSONObject();
+    private boolean isMock = false;
     private com.sandata.MockGpsChecker mContext;
 
 	
@@ -35,28 +34,22 @@ public class MockLocationChecker extends CordovaPlugin{
     public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
 	
         if (action.equals("check")) {
-	    cordova.getThreadPool().execute(new Runnable() {public void run() {
             if (android.os.Build.VERSION.SDK_INT < 18) {
                 if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")){
-                    objGPS.put("isMock",false);
+                    isMock = false;
                 } else {
-                    objGPS.put("isMock",true);
+                    isMock = true;
                 }
             } else {
                 if (location.isFromMockProvider() == true) {
-                    objGPS.put("isMock",false);
+                    isMock = true;
                 } else {
-                    objGPS.put("isMock",true);
+                    isMock = false;
                 }
             }
-			Log.i("Location", "isMock: "+objGPS.get("isMock"));
-			System.println("isMock>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+objGPS.get("isMock"));
-            callbackContext.success(objGPS);
+			callbackContext.success(isMock);
 			return true;
-	    }})    
         } else {
-			System.println("MAMPUSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-
 			return false;
         }
 
